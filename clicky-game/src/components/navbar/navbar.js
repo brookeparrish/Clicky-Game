6a66d8c6a244250ec6aa2f6f51220e31253b635f
Brooke <parrish.e.brooke@gmail.com>
2020-01-12 14:16:01 -0700

    got nav bar positioned correctly

--- clicky-game/src/components/navbar/navbar.js
+++ clicky-game/src/components/navbar/navbar.js
@@ -3,11 +3,25 @@ import './Navbar.css';
 
 const Navbar = (props) => {
     return (
-        <ul>
-            <li>Clicky Game</li>
-            <li>Click an image to begin!</li>
-            <li>Score:</li>
-        </ul>
+        <div className="row w-100 pb-4">
+            <div className="col text-center">
+                Clicky Game
+            </div>
+                
+            <div className="col text-center">
+                Click an image to begin!
+            </div>
+
+            <div className="col text-center">
+                Score:
+            </div>
+        </div>
+
+        // <ul className="list list-group list-group-horizontal">
+        //     <li className="list-group-item">Clicky Game</li>
+        //     <li className="list-group-item">Click an image to begin!</li>
+        //     <li className="list-group-item">Score:</li>
+        // </ul>
     );
 };
 
